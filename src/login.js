95041254d579251a32007cad6e6842770abc0701
gopal <kundetigopal18@gmail.com>
2024-08-13 19:24:23 +0530

    login.js

---
+++ src/login.js
@@ -0,0 +1,12 @@
+function Login() {
+    return(
+        <div>
+            <h4>Login Details</h4>
+        <label>Username or Email:</label>&nbsp;
+        <input type="text" placeholder="&nbsp; Username &nbsp; or &nbsp; Email" />
+        <label>Password :</label>&nbsp;
+        <input type="text" placeholder="&nbsp; Password &nbsp;" />
+        </div>
+         );
+    
+}
