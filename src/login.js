2edf3b6a373a7ae0e2c6bbba5d9d3e6fac7b7d58
kundeti18 <kundetigopal18@gmail.com>
2024-08-13 15:21:25 +0530

    Add files via upload

---
+++ src/login.js
@@ -0,0 +1,10 @@
+function Login() {
+    return(
+        <div>
+            <h4>Login Details</h4>
+        <label>Username or Email:</label>&nbsp;
+        <input type="text" placeholder="&nbsp; Username &nbsp; or &nbsp; Email" />
+        </div>
+         );
+    
+}
